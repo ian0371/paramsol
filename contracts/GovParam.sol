809a53aefdfa5b1f1eb2a1cda1b3c787cd20dd0d
Chihyun Song <ian0371@gmail.com>
2022-03-18 18:01:32 +0900

    Add addParam/getParam

--- contracts/GovParam.sol
+++ contracts/GovParam.sol
@@ -5,10 +5,41 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GovParam is Ownable {
+    address public voteContract;
+    mapping(uint => Param) public params;
+    uint[] public paramIds;
+
+    struct Param {
+        string  name;        // ex) "istanbul.epoch"
+        bool    votable;     // true: owner&voter can change, false: only owner
+        uint64  fromBlock;   // block number for last change
+        bytes   prevValue;   // RLP encoded value before nextBlock
+        bytes   nextValue;   // RLP encoded value after nextBlock
+    }
+
     constructor(address _owner) {
         if (_owner != address(0)) {
             console.log("Transferring ownership to", _owner);
             transferOwnership(_owner);
+        }
+    }
+
+    function addParam(uint id, string calldata _name, bool _votable, bytes calldata value) public {
+        params[id] = Param({
+            name: _name,
+            votable: _votable,
+            fromBlock: 0, // or block.number
+            prevValue: "",  // unused
+            nextValue: value
+        });
+    }
+
+    function getParam(uint id) external view returns (bytes memory) {
+        if (block.number >= params[id].fromBlock) {
+            return params[id].nextValue;
+        }
+        else {
+            return params[id].prevValue;
         }
     }
 }
--- test/test.js
+++ test/test.js
@@ -11,3 +11,21 @@ describe("constructor", function () {
     expect(await gp.owner()).to.equal(accounts[1].address);
   });
 });
+
+describe("addParam", function () {
+  let gp;
+
+  beforeEach(async function () {
+    const accounts = await hre.ethers.getSigners();
+    const GovParam = await ethers.getContractFactory("GovParam");
+    gp = await GovParam.deploy(accounts[1].address);
+    await gp.deployed();
+  });
+
+  it("addParam success", async function () {
+    epoch = ethers.utils.RLP.encode('0x093a80');
+    await gp.addParam(0, "istanbul.epoch", false, epoch);
+    p = await gp.getParam(0);
+    expect(p).to.equal(epoch);
+  });
+});
